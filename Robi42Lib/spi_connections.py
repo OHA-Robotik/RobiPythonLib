1d82fb21bb5c3ae99df91feb98f96aa99ec3c64e
Fabian Schneebauer <fabian@fs-it.org>
2023-04-16 19:11:50 +0200

    Add analog support.

--- Robi42Lib/spi_connections.py
+++ Robi42Lib/spi_connections.py
@@ -12,3 +12,16 @@ led_and_motor_spi = SPI(
     mosi=Pin(11),
     miso=Pin(12),
 )
+
+analog_cs = Pin(5, Pin.OUT)
+analog_spi = SPI(
+    0,
+    baudrate=1000000,
+    polarity=1,
+    phase=1,
+    bits=8,
+    firstbit=SPI.MSB,
+    sck=Pin(2),
+    mosi=Pin(3),
+    miso=Pin(4),
+)
