f1087a6ca7161f40ebb1a4ba103eb230faacf944
Finn Drünert <finndruenert@gmail.com>
2023-04-16 20:00:28 +0200

    public read raw

--- Robi42Lib/poti.py
+++ Robi42Lib/poti.py
@@ -5,11 +5,11 @@ class Poti:
     def __init__(self) -> None:
         pass
 
-    def _get_raw_value(self) -> int:
+    def get_raw_value(self) -> int:
         return analog_mcp.read(6)
 
     def get_value(self) -> float:
-        raw = self._get_raw_value()
+        raw = self.get_raw_value()
         if raw < 50:
             raw = 0
         elif raw > 974:
