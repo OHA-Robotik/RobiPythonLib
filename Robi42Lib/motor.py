f73b4d690f8dc364bab957ffbd2566565ee96dde
Finn Drünert <finndruenert@gmail.com>
2023-04-16 19:03:33 +0200

    methods for acceleration

--- Robi42Lib/motor.py
+++ Robi42Lib/motor.py
@@ -1,12 +1,15 @@
 from Robi42Lib.mcps import motor_and_button_mcp
 from machine import Pin, PWM
+from time import sleep_ms
 
 
 class MotorLeft:
+    current_freq: int
+
     def __init__(self) -> None:
         self.disable()
         self.step_pwm = PWM(Pin(20, Pin.OUT))
-        self.step_pwm.freq(420)
+        self.set_freq(420)
         self.step_pwm.duty_u16(32768)
         self.set_stepping_size(1, 1, 1)
         self.set_direction(1)
@@ -18,6 +21,7 @@ class MotorLeft:
         motor_and_button_mcp.digital_write(14, 1)
 
     def set_freq(self, freq: int):
+        self.current_freq = freq
         self.step_pwm.freq(freq)
 
     def set_stepping_size(self, m0: bool, m1: bool, m2: bool):
@@ -29,6 +33,23 @@ class MotorLeft:
     def set_direction(self, direction: bool):
         direction = bool(direction)
         motor_and_button_mcp.digital_write(3, direction)
+
+    def accelerate_to_freq(self, freq: int, hz_per_second: int):
+        freq_dif = freq - self.current_freq
+        delay_ms = 100
+        step_size = int(hz_per_second * (delay_ms / 1000))
+
+        if freq_dif > 0:
+            for f in range(self.current_freq + step_size, freq, step_size):
+                self.set_freq(f)
+                sleep_ms(delay_ms)
+
+            self.set_freq(freq)
+
+        elif freq_dif < 0:
+            for f in range(freq, self.current_freq, step_size)[::-1]:
+                self.set_freq(f)
+                sleep_ms(delay_ms)
 
 
 class MotorRight:
@@ -59,11 +80,28 @@ class MotorRight:
         direction = bool(direction)
         motor_and_button_mcp.digital_write(7, not direction)
 
+    def accelerate_to_freq(self, freq: int, hz_per_second: int):
+        freq_dif = freq - self.current_freq
+        delay_ms = 100
+        step_size = int(hz_per_second * (delay_ms / 1000))
+
+        if freq_dif > 0:
+            for f in range(self.current_freq + step_size, freq, step_size):
+                self.set_freq(f)
+                sleep_ms(delay_ms)
+
+            self.set_freq(freq)
+
+        elif freq_dif < 0:
+            for f in range(freq, self.current_freq, step_size)[::-1]:
+                self.set_freq(f)
+                sleep_ms(delay_ms)
+
 
 class Motors:
-
     dir_forward = True
     dir_backward = False
+    current_freq = 420
 
     def __init__(self) -> None:
         self.left = MotorLeft()
@@ -78,6 +116,7 @@ class Motors:
         self.right.enable()
 
     def set_freq(self, freq: int):
+        self.current_freq = freq
         self.left.set_freq(freq)
         self.right.set_freq(freq)
 
@@ -88,3 +127,20 @@ class Motors:
     def set_direction(self, direction: bool):
         self.left.set_direction(direction)
         self.right.set_direction(direction)
+
+    def accelerate_to_freq(self, freq: int, hz_per_second: int):
+        freq_dif = freq - self.current_freq
+        delay_ms = 100
+        step_size = int(hz_per_second * (delay_ms / 1000))
+
+        if freq_dif > 0:
+            for f in range(self.current_freq + step_size, freq, step_size):
+                self.set_freq(f)
+                sleep_ms(delay_ms)
+
+            self.set_freq(freq)
+
+        elif freq_dif < 0:
+            for f in range(freq, self.current_freq, step_size)[::-1]:
+                self.set_freq(f)
+                sleep_ms(delay_ms)
