59a1716e6f283bbe3c5f68222b5e2e43d8de89c9
Finnomator <finndruenert@gmail.com>
2023-05-03 15:10:45 +0200

    hold down the center button to go to admin menu

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from Robi42Lib.robi42 import Robi42
 
-with Robi42() as r:
-    pass
+
+with Robi42(
+    enable_gyro=False,
+    enable_ir_sensors=False,
+    enable_laser_sensor=False,
+    enable_motors=False,
+) as r:
+    if r.buttons.center.is_pressed():
+        from Robi42Lib.AdminMenu import admin_menu
+
+        admin_menu.MainMenu(r).goto()
+    else:
+        # TODO: only init these if they are connected
+        r.init_gyro()
+        r.init_ir_sensors()
+        r.init_laser_sensor()
+        r.init_motors()
