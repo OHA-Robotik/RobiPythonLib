43e35f0d8aa031c7aaf469403c1772853c73a914
Julian Ruß <z@fsei.de>
2023-07-31 14:33:45 +0200

    BF

--- Robi42Lib/modules/buttons.py
+++ Robi42Lib/modules/buttons.py
@@ -4,8 +4,8 @@ from . import base_module
 class Buttons(base_module.BaseModule):
 
     def __init__(self) -> None:
-        self.up = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_up)
-        self.down = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_down)
-        self.center = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_center)
-        self.left = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_left)
-        self.right = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_right)
+        self.up = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_up, pull=base_module.DigitalBoardPin.PULL_UP)
+        self.down = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_down, pull=base_module.DigitalBoardPin.PULL_UP)
+        self.center = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_center, pull=base_module.DigitalBoardPin.PULL_UP)
+        self.left = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_left, pull=base_module.DigitalBoardPin.PULL_UP)
+        self.right = base_module.DigitalBoardPin(base_module.DigitalBoardPins.btn_right, pull=base_module.DigitalBoardPin.PULL_UP)
