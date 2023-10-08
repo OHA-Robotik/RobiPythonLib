2aaef28d41d0401205f68a4618df20db8231713c
Fabian Schneebauer <fabian@fs-it.org>
2023-10-08 22:21:21 +0200

    Unfinished gyro

--- Robi42Lib/modules/gyro.py
+++ Robi42Lib/modules/gyro.py
@@ -9,52 +9,52 @@ class Gyro(base_module.BaseModule):
     __last_roll = 0
     __prev_time = utime.ticks_ms()
 
-    @base_module.need_i2c_hardware('mpu6050')
-    def get_temperature(self, mpu6050=None) -> float:
+    @base_module.get_first_i2c_hardware('MPU6050')
+    def get_temperature(self, MPU6050=None) -> float:
         """
         Returns the temperature in °C
         """
-        return mpu6050.get_temperature()
+        return MPU6050.temperature
 
-    @base_module.need_i2c_hardware('mpu6050')
-    def get_acceleration(self, mpu6050=None) -> tuple[float, float, float]:
+    @base_module.get_first_i2c_hardware('MPU6050')
+    def get_acceleration(self, MPU6050=None) -> tuple[float, float, float]:
         """
         Returns the acceleration in g
         """
-        return mpu6050.get_acceleration()
+        return MPU6050.get_acceleration()
 
-    @base_module.need_i2c_hardware('mpu6050')
-    def get_gyro(self, mpu6050=None) -> tuple[float, float, float]:
+    @base_module.get_first_i2c_hardware('MPU6050')
+    def get_gyro(self, MPU6050=None) -> tuple[float, float, float]:
         """
         Returns the gyroscope in °/s
         """
-        return mpu6050.get_gyro()
+        return MPU6050.get_gyro()
 
-    @base_module.need_i2c_hardware('mpu6050')
+    @base_module.get_first_i2c_hardware('MPU6050')
     def get_all_data(
         self,
-        mpu6050=None,
+        MPU6050=None,
     ) -> dict[
         str:float,
         str : dict[str:float, str:float, str:float],
         str : dict[str:float, str:float, str:float],
     ]:
-        return mpu6050.get_data()
+        return MPU6050.get_data()
 
-    @base_module.need_i2c_hardware('mpu6050')
-    def get_tilt_angles(self, mpu6050=None) -> tuple[float, float, float]:
+    @base_module.get_first_i2c_hardware('MPU6050')
+    def get_tilt_angles(self, MPU6050=None) -> tuple[float, float, float]:
         """
         Calculates the rotation in degree
         """
-        return mpu6050.calculate_tilt_angles(*self.get_acceleration())
+        return MPU6050.calculate_tilt_angles(*self.get_acceleration())
 
-    @base_module.need_i2c_hardware('mpu6050')
-    def get_pitch_and_roll(self, mpu6050=None) -> tuple[float, float]:
+    @base_module.get_first_i2c_hardware('MPU6050')
+    def get_pitch_and_roll(self, MPU6050=None) -> tuple[float, float]:
         curr_time = utime.ticks_ms()
         dt = (curr_time - self.__prev_time) / 1000
         gyro = self.get_gyro()
-        pitch = mpu6050.get_pitch(self.__last_pitch, gyro, dt)
-        roll = mpu6050.get_roll(self.__last_roll, gyro, dt)
+        pitch = MPU6050.get_pitch(self.__last_pitch, gyro, dt)
+        roll = MPU6050.get_roll(self.__last_roll, gyro, dt)
         self.__prev_time = curr_time
         return pitch, roll
 
--- Robi42Lib/robi42.py
+++ Robi42Lib/robi42.py
@@ -7,6 +7,7 @@ from .modules import poti as _mod_poti
 from .modules import led as _mod_led
 from .modules import ir_sensor as _mod_ir_sensor
 from .modules import lcd as _mod_lcd
+from .modules import gyro as _mod_gyro
 from .modules import laser_sensor as _mod_laser_sensor
 
 
@@ -21,3 +22,4 @@ class Robi42:
         self.ir_sensors = _mod_ir_sensor.IrSensors()
         self.lcd = _mod_lcd.LCD()
         self.laser_sensor = _mod_laser_sensor.LaserSensor()
+        self.gyro = _mod_gyro.Gyro()
