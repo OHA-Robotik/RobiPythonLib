f2b42ee8e7a214f0ac8909a33fef2b5d23cf6b90
Finn Drünert <finndruenert@gmail.com>
2023-10-08 21:05:39 +0200

    BF

--- Robi42Lib/modules/base_module.py
+++ Robi42Lib/modules/base_module.py
@@ -1,7 +1,6 @@
 from ..hardware_manager import manager as hw_manager
 from ..hardware_manager import platform_description as pltfm_desc
 
-
 # redefine for easy access the Hardware
 DigitalBoardPin = pltfm_desc.DigitalBoardPin
 DigitalBoardPins = pltfm_desc.DigitalBoardPins
@@ -18,7 +17,9 @@ def needs_i2c_hardware(driver_name):
             func(*args, **kwargs, **{driver_name: driver})
 
         return inner_wrap
+
     return function_wrapper
+
 
 def get_first_i2c_hardware(driver_name, ignore_if_not_present=True):
     def function_wrapper(func):
@@ -27,12 +28,13 @@ def get_first_i2c_hardware(driver_name, ignore_if_not_present=True):
         def inner_wrap(*args, **kwargs):
             drivers = manager_instance.get_i2c_driver_by_name(driver_name)
             if len(drivers) > 0:
-                func(*args, **kwargs, **{driver_name: drivers[0]})
-            else:
-                if not ignore_if_not_present:
-                    raise RuntimeError('Hardware for driver \'{}\' not registered!'.format(driver_name))
+                return func(*args, **kwargs, **{driver_name: drivers[0]})
+
+            if not ignore_if_not_present:
+                raise RuntimeError('Hardware for driver \'{}\' not registered!'.format(driver_name))
 
         return inner_wrap
+
     return function_wrapper
 
 
