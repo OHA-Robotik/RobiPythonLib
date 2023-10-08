e75738149cfa5d1213782bc4ce2a490e2af22c32
Finn Drünert <finndruenert@gmail.com>
2023-10-08 19:51:41 +0200

    implement leds

--- Robi42Lib/modules/led.py
+++ Robi42Lib/modules/led.py
@@ -3,36 +3,49 @@ from . import base_module
 
 class LedGroup:
     def __init__(self, leds: list[base_module.DigitalBoardPin]):
-        self.leds = leds
+        self.__pins = leds
 
     def on(self):
-        for led in self.leds:
-            led.on()
+        for led in self.__pins:
+            # Because "Gegen Masse"
+            led.off()
 
     def off(self):
-        for led in self.leds:
-            led.off()
+        for led in self.__pins:
+            # Because "Gegen Masse"
+            led.on()
+
+    def toggle(self):
+        for led in self.__pins:
+            led.value(not led.value())
+
+    def value(self, level: bool):
+        for led in self.__pins:
+            led.value(level)
 
 
 class Leds(base_module.BaseModule):
     def __init__(self) -> None:
-        self.headlight_left = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_sl)])
-        self.headlight_right = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_sr)])
-        self.blinker_front_left = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_lv)])
-        self.blinker_front_right = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_rv)])
-        self.blinker_back_left = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_lh)])
-        self.blinker_back_right = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_rh)])
-        self.backlight_left = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_rl)])
-        self.backlight_right = LedGroup([base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_rr)])
-        self.headlights = LedGroup([self.headlight_left.leds[0], self.headlight_right.leds[0]])
-        self.backlights = LedGroup([self.backlight_left.leds[0], self.backlight_right.leds[0]])
-        self.all = LedGroup([
-            self.blinker_front_left.leds[0],
-            self.headlight_left.leds[0],
-            self.headlight_right.leds[0],
-            self.blinker_front_right.leds[0],
-            self.blinker_back_right.leds[0],
-            self.backlight_right.leds[0],
-            self.backlight_left.leds[0],
-            self.blinker_back_left.leds[0],
-        ])
+        h_l_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_sl)
+        h_r_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_sr)
+
+        b_l_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_rl)
+        b_r_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_rr)
+
+        bl_fl_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_lv)
+        bl_fr_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_rv)
+        bl_bl_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_lh)
+        bl_br_pin = base_module.DigitalBoardPin(base_module.DigitalBoardPins.led_bl_rh)
+
+        self.headlight_left = LedGroup([h_l_pin])
+        self.headlight_right = LedGroup([h_r_pin])
+        self.blinker_front_left = LedGroup([bl_fl_pin])
+        self.blinker_front_right = LedGroup([bl_fr_pin])
+        self.blinker_back_left = LedGroup([bl_bl_pin])
+        self.blinker_back_right = LedGroup([bl_br_pin])
+        self.backlight_left = LedGroup([b_l_pin])
+        self.backlight_right = LedGroup([b_r_pin])
+        self.headlights = LedGroup([h_l_pin, h_r_pin])
+        self.backlights = LedGroup([b_l_pin, b_r_pin])
+        self.blinkers = LedGroup([bl_fl_pin, bl_fr_pin, bl_bl_pin, bl_br_pin])
+        self.all = LedGroup([h_l_pin, h_r_pin, b_l_pin, b_r_pin, bl_fl_pin, bl_fr_pin, bl_bl_pin, bl_br_pin])
--- Robi42Lib/robi42.py
+++ Robi42Lib/robi42.py
@@ -4,6 +4,7 @@ from .modules import buttons as _mod_buttons
 from .modules import piezo as _mod_piezo
 from .modules import motor as _mod_motor
 from .modules import poti as _mod_poti
+from .modules import led as _mod_led
 
 
 class Robi42:
@@ -13,3 +14,4 @@ class Robi42:
         self.piezo = _mod_piezo.Piezo()
         self.motors = _mod_motor.Motors()
         self.poti = _mod_poti.Poti()
+        self.leds = _mod_led.Leds()
