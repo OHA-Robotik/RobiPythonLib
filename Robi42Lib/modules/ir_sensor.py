7365ab4e2e73be90b544b54bdfd4913f7381337d
Finn Drünert <finndruenert@gmail.com>
2023-10-08 20:29:55 +0200

    separate ir sensors

--- Robi42Lib/modules/ir_sensor.py
+++ Robi42Lib/modules/ir_sensor.py
@@ -9,11 +9,22 @@ def convert(raw: int) -> float:
     return raw / 0b1111111111
 
 
+class _IrSensor:
+    def __init__(self, pin: base_module.AnalogBoardPin):
+        self.__pin = pin
+
+    def read_raw(self) -> int:
+        return self.__pin.read_raw()
+
+    def read_value(self) -> float:
+        return convert(self.read_raw())
+
+
 class IrSensors(base_module.BaseModule):
     def __init__(self) -> None:
-        self.left = base_module.AnalogBoardPin(base_module.AnalogBoardPins.ir_left)
-        self.middle = base_module.AnalogBoardPin(base_module.AnalogBoardPins.ir_middle)
-        self.right = base_module.AnalogBoardPin(base_module.AnalogBoardPins.ir_right)
+        self.left = _IrSensor(base_module.AnalogBoardPin(base_module.AnalogBoardPins.ir_left))
+        self.middle = _IrSensor(base_module.AnalogBoardPin(base_module.AnalogBoardPins.ir_middle))
+        self.right = _IrSensor(base_module.AnalogBoardPin(base_module.AnalogBoardPins.ir_right))
 
     def read_raw_values(self) -> tuple[int, int, int]:
         return (
