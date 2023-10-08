80f0f3bc95f4ce1b1d64f5d24e47682184dd4e7b
Finn Drünert <finndruenert@gmail.com>
2023-10-08 18:02:46 +0200

    import bf

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from Robi42Lib.robi42 import Robi42
+from Robi42Lib import robi42
+from Robi42Lib.modules.piezo import SampleTones
 
-
-with Robi42(
-    enable_gyro=False,
-    enable_ir_sensors=False,
-    enable_laser_sensor=False,
-    enable_motors=False,
-) as r:
-    if r.buttons.center.is_pressed():
-        from Robi42Lib.AdminMenu import admin_menu
-
-        admin_menu.MainMenu(r).goto()
-    else:
-        # TODO: only init these if they are connected
-        r.init_gyro()
-        r.init_ir_sensors()
-        r.init_laser_sensor()
-        r.init_motors()
+r = robi42.Robi42()
+r.piezo.play_tone(SampleTones.get_tone("C0"))
