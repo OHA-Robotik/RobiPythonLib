1275aa47624f8f8bd5c60c856ae8f549d083c586
Finn Drünert <finndruenert@gmail.com>
2023-10-08 18:03:13 +0200

    rename args to kwargs

--- Robi42Lib/modules/base_module.py
+++ Robi42Lib/modules/base_module.py
@@ -12,9 +12,11 @@ AnalogBoardPins = pltfm_desc.AnalogBoardPins
 def needs_i2c_hardware(driver_name):
     def function_wrapper(func):
         manager_instance = hw_manager.HardwareManager.get_instance()
+
         def inner_wrap(*args, **kwargs):
             driver = manager_instance.get_i2c_driver_by_name(driver_name)
-            func(*args, **args, **{driver_name: driver})
+            func(*args, **kwargs, **{driver_name: driver})
+
         return inner_wrap
     return function_wrapper
 
