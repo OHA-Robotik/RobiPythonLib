1d00dbcfb5415927b49998a1533f001380311c62
Finn Drünert <finndruenert@gmail.com>
2023-10-08 18:43:49 +0200

    implement motors

--- Robi42Lib/robi42.py
+++ Robi42Lib/robi42.py
@@ -2,6 +2,7 @@ from time import sleep as _sleep
 
 from .modules import buttons as _mod_buttons
 from .modules import piezo as _mod_piezo
+from .modules import motor as _mod_motor
 
 
 class Robi42:
@@ -9,3 +10,4 @@ class Robi42:
     def __init__(self,):
         self.buttons = _mod_buttons.Buttons()
         self.piezo = _mod_piezo.Piezo()
+        self.motors = _mod_motor.Motors()
