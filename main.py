fa0d06413c8b5fd8537fbb38ede041a67fb4c1d6
Finn Drünert <finndruenert@gmail.com>
2026-02-14 16:26:59 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from Robi42Lib import robi42
-from Robi42Lib.modules.piezo import SampleTones
+from time import sleep
 
 r = robi42.Robi42()
-r.piezo.play_tone(SampleTones.get_tone("C0"))
+r.begin()
+
+r.leds.all.on()
+
+r.lcd.turn_on()
+
+output = "Hello!"
+
+if r.external_storage.is_connected():
+    output += f"\nHW Rev.: {r.external_storage.read_hw_revision()}"
+
+r.lcd.put_str(output)
+
+while True:
+    sleep(1)
