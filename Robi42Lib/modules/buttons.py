ca6665ad2535ea1704a5cb432cb2e2789931179e
Finn Drünert <finndruenert@gmail.com>
2026-02-15 08:44:38 +0100

    buttons

--- Robi42Lib/modules/buttons.py
+++ Robi42Lib/modules/buttons.py
@@ -1,31 +1,20 @@
 from . import base_module
 
 
+class Button(base_module.DigitalBoardPin):
+
+    def __init__(self, pin_id: str):
+        super().__init__(pin_id, base_module.DigitalBoardPin.IN, base_module.DigitalBoardPin.PULL_UP)
+
+    def is_pressed(self):
+        return not self.value()
+
+
 class Buttons(base_module.BaseModule):
 
     def __init__(self) -> None:
-        self.up = base_module.DigitalBoardPin(
-            base_module.DigitalBoardPins.btn_up,
-            mode=base_module.DigitalBoardPin.IN,
-            pull=base_module.DigitalBoardPin.PULL_UP,
-        )
-        self.down = base_module.DigitalBoardPin(
-            base_module.DigitalBoardPins.btn_down,
-            mode=base_module.DigitalBoardPin.IN,
-            pull=base_module.DigitalBoardPin.PULL_UP,
-        )
-        self.center = base_module.DigitalBoardPin(
-            base_module.DigitalBoardPins.btn_center,
-            mode=base_module.DigitalBoardPin.IN,
-            pull=base_module.DigitalBoardPin.PULL_UP,
-        )
-        self.left = base_module.DigitalBoardPin(
-            base_module.DigitalBoardPins.btn_left,
-            mode=base_module.DigitalBoardPin.IN,
-            pull=base_module.DigitalBoardPin.PULL_UP,
-        )
-        self.right = base_module.DigitalBoardPin(
-            base_module.DigitalBoardPins.btn_right,
-            mode=base_module.DigitalBoardPin.IN,
-            pull=base_module.DigitalBoardPin.PULL_UP,
-        )
+        self.up = Button(base_module.DigitalBoardPins.btn_up)
+        self.down = Button(base_module.DigitalBoardPins.btn_down)
+        self.center = Button(base_module.DigitalBoardPins.btn_center, )
+        self.left = Button(base_module.DigitalBoardPins.btn_left, )
+        self.right = Button(base_module.DigitalBoardPins.btn_right, )
